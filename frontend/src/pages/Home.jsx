b1c3aebebc72a78a2c6f26732a6e073d2f3449ad
Islomjon <ergashevislomjon1020@gmail.com>
2024-01-14 19:50:01 +0500

    fix: mobile accsess

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -35,52 +35,59 @@ export default function Home() {
     }
   };
   return (
-    <div className="w-full h-screen flex justify-center items-center text-gray-100">
-      <div className="max-w-md rounded-xl w-full h-fit flex flex-col px-8 py-6 bg-[#1E1E1E]">
-        <h4 className="text-lg font-semibold mb-2">Paste invitation Room ID</h4>
-        <div className="flex flex-col gap-3">
-          <input
-            type="text"
-            className="rounded px-3 py-1.5 h-10 bg-transparent border border-gray-200 outline-blue-500"
-            placeholder="Enter Room ID"
-            onChange={e => setRoomId(e.target.value)}
-            value={roomId}
-            onKeyUp={handleInputEnter}
-          />
-          <input
-            type="text"
-            className="rounded px-3 py-1.5 h-10 bg-transparent border border-gray-200 outline-blue-500"
-            placeholder="Your Username"
-            onChange={e => setUsername(e.target.value)}
-            value={username}
-            onKeyUp={handleInputEnter}
-          />
-          <div className="flex gap-2 items-center justify-between">
-            <button
-              className="bg-[#2D2D2D] w-1/2 px-5 h-10 text-sm  border hover:border-gray-600 border-transparent rounded transition duration-300 active:bg-gray-600"
-              onClick={joinRoom}
-            >
-              Join Room
-            </button>
-            <button
-              className="bg-[#2D2D2D] w-1/2 px-5 h-10 text-sm  border hover:border-gray-600 border-transparent rounded transition duration-300 active:bg-gray-600"
-              onClick={createNewRoom}
-            >
-              Create Room
-            </button>
-          </div>
-          <h4 className="text-sm text-center">
-            Created by{" "}
-            <Link
-              to={"https://t.me/islomjon5747"}
-              target="_blank"
-              className="text-blue-800 font-semibold"
-            >
-              Islomjon
-            </Link>
+    <>
+      <div className="w-full h-screen flex justify-center items-center text-gray-100 max-xl:hidden">
+        <div className="max-w-md rounded-xl w-full h-fit flex flex-col px-8 py-6 bg-[#1E1E1E]">
+          <h4 className="text-lg font-semibold mb-2">
+            Paste invitation Room ID
           </h4>
+          <div className="flex flex-col gap-3">
+            <input
+              type="text"
+              className="rounded px-3 py-1.5 h-10 bg-transparent border border-gray-200 outline-blue-500"
+              placeholder="Enter Room ID"
+              onChange={e => setRoomId(e.target.value)}
+              value={roomId}
+              onKeyUp={handleInputEnter}
+            />
+            <input
+              type="text"
+              className="rounded px-3 py-1.5 h-10 bg-transparent border border-gray-200 outline-blue-500"
+              placeholder="Your Username"
+              onChange={e => setUsername(e.target.value)}
+              value={username}
+              onKeyUp={handleInputEnter}
+            />
+            <div className="flex gap-2 items-center justify-between">
+              <button
+                className="bg-[#2D2D2D] w-1/2 px-5 h-10 text-sm  border hover:border-gray-600 border-transparent rounded transition duration-300 active:bg-gray-600"
+                onClick={joinRoom}
+              >
+                Join Room
+              </button>
+              <button
+                className="bg-[#2D2D2D] w-1/2 px-5 h-10 text-sm  border hover:border-gray-600 border-transparent rounded transition duration-300 active:bg-gray-600"
+                onClick={createNewRoom}
+              >
+                Create Room
+              </button>
+            </div>
+            <h4 className="text-sm text-center">
+              Created by{" "}
+              <Link
+                to={"https://t.me/islomjon5747"}
+                target="_blank"
+                className="text-blue-800 font-semibold"
+              >
+                Islomjon
+              </Link>
+            </h4>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="xl:hidden">
+        <h4 className="text-center text-white flex h-screen justify-center items-center px-2">Afsuski telefondan dasturga kira olmaysiz {"("}:</h4>
+      </div>
+    </>
   );
 }
