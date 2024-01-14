5e39cb6f0a1984c3dbc976cffbfa1e39c26cc162
Islomjon <ergashevislomjon1020@gmail.com>
2024-01-14 18:30:20 +0500

    fix: server

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,12 +1,11 @@
 import { io } from 'socket.io-client';
 
 export const initSocket = async () => {
-    console.log(import.meta.env.VITE_APP_BACKEND_URL);
     const options = {
         'force new connection': true,
         reconnectionAttempt: 'Infinity',
         timeout: 10000,
         transports: ['websocket'],
     };
-    return io(import.meta.env.VITE_APP_BACKEND_URL, options);
+    return io("https://realtime-codeshare.onrender.com", options);
 };
